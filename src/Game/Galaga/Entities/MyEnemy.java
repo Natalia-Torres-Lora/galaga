86cfbfaf49ed8693006d4bccc1c97d9bb4dd4470
nataliatorres <nataliatorres@10.31.2.81>
2020-02-22 15:43:39 -0400

    Created class for new enemy bee

---
+++ src/Game/Galaga/Entities/MyEnemy.java
@@ -0,0 +1,12 @@
+package Game.Galaga.Entities;
+
+import Main.Handler;
+import Resources.Animation;
+import Resources.Images;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MyEnemy {
+
+}
